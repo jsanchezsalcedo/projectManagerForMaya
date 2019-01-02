efc0ea75e361a596832ca20b6ebbac9d0afcaade
Jorge Sánchez Salcedo <jorgesanchez.da@gmail.com>
2019-01-02 11:33:11 +0100

    Order changed
    Directories order changed for a better functionality

--- projectManager.py
+++ projectManager.py
@@ -1,7 +1,7 @@
 import os
 
 projects = []
-departments = ['model', 'lookdev', 'rig', 'layout', 'anim', 'fx', 'lighting']
+departments = []
 dirA = []
 dirB = []
 dirC = []
@@ -24,21 +24,14 @@ class ProjectManager(dict):
             pass
         return projects
 
-    def departmentsDir(self):
-        if os.getenv('DPT') == None:
-            os.environ['DPT'] = departments[0]
-        else:
-            os.getenv('DPT')
-        return departments
-
     def directoryA(self):
         dirA = []
-        preprod = ['model', 'lookdev', 'rig']
+        projectPath = os.path.join(os.getenv('ROOT'), os.getenv('PRJ'))
         try:
-            if os.getenv('DPT') in preprod:
-                dirA = ['assets', 'libs']
-            else:
-                dirA = ['seqs']
+            for i in os.listdir(projectPath):
+                path = os.path.join(projectPath, i)
+                if os.path.isdir(path):
+                    dirA.append(i)
             if os.getenv('DIR_A') == None:
                 os.environ['DIR_A'] = dirA[0]
             else:
@@ -79,10 +72,31 @@ class ProjectManager(dict):
         except WindowsError:
             pass
 
+    def departmentsDir(self):
+        departments = []
+        departmentsPath = os.path.join(os.getenv('ROOT'), os.getenv('PRJ'), os.getenv('DIR_A'), os.getenv('DIR_B'), os.getenv('DIR_C'))
+        try:
+            for i in os.listdir(departmentsPath):
+                path = os.path.join(departmentsPath, i)
+                if os.path.isdir(path):
+                    departments.append(i)
+            if os.getenv('DPT') == None:
+                os.environ['DPT'] = departments[0]
+            else:
+                pass
+            return departments
+        except WindowsError:
+            pass
+
     def getProject(self):
         projectPath = os.path.join(os.getenv('ROOT'), os.getenv('PRJ'), os.getenv('DIR_A'), os.getenv('DIR_B'), os.getenv('DIR_C'), os.getenv('DPT'))
         filesPath = os.path.join(projectPath, 'version')
         return projectPath, filesPath
+
+    def getBackups(self):
+        projectPath, filesPath = self.getProject()
+        backupPath = os.path.join(projectPath, 'tmp')
+        return backupPath
 
     def melFiles(self):
         projectPath, filesPath = self.getProject()
@@ -104,5 +118,12 @@ class ProjectManager(dict):
         except WindowsError:
             pass
 
+    def backupFiles(self):
+        backupPath = self.getBackups()
+        try:
+            files = os.listdir(backupPath)
+            backupFiles = [f for f in files if f.endswith('.ma')]
+            return backupFiles
 
-
+        except WindowsError:
+            pass
